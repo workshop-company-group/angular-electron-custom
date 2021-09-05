704816db66d64b6bd38ee5edf14c99a6b1935e2f
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-09-05 13:39:01 +0300

    remove webview directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { PageNotFoundComponent } from './components/';
-import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     PageNotFoundComponent,
-    WebviewDirective,
   ],
   imports: [
     CommonModule,
@@ -19,7 +17,6 @@ import { FormsModule } from '@angular/forms';
   ],
   exports: [
     TranslateModule,
-    WebviewDirective,
     FormsModule,
   ],
 })
